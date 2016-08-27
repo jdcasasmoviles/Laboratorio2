e89dd0475212000fcbcb2dfe4873dab8426b02fb
jdcasasmoviles <jdcasasmoviles@gmail.com>
2016-08-27 11:09:07 -0500

    Entorno de desarrollo: Android Studio

---
+++ Labo2Acti4/app/src/main/java/jdcasas/labo2acti4/Modelo.java
@@ -0,0 +1,9 @@
+package jdcasas.labo2acti4;
+
+/**
+ * Created by Usuario on 25/08/2016.
+ */
+public class Modelo {
+    final static String cad1="db_usuarios_acceso";
+    final static String cad2="col_nombre_usuario";
+}
---
+++ Labo2Acti4/app/src/main/java/jdcasas/labo2acti4/Prueba.java
@@ -0,0 +1,31 @@
+package jdcasas.labo2acti4;
+
+/**
+ * Created by Usuario on 25/08/2016.
+ */
+public class Prueba {
+    private String dato;
+    String cadena=Modelo.cad1;
+
+    public Prueba(){}
+
+    public void ejecutar(){
+    String var=dato;
+        for(int i=1;i<=10;i++){
+
+        }
+
+        if(dato.length()>0){}
+        for(char k : dato.toCharArray()){
+
+        }
+    }
+
+    public String getDato() {
+        return dato;
+    }
+
+    public void setDato(String dato) {
+        this.dato = dato;
+    }
+}
